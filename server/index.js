1d45e4c1be6ad0c09d9c17288755757e7f0d83e2
Md Arieeb Ali <arieebali@gmail.com>
2026-03-31 19:15:50 +0530

    Set up Express server with CORS and routes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
+import express from "express;
+import cors from "cors";
+import dotenv from "dotenv";
+import transactionsRoute from ".routes/transactions.js";
+ dotenv.config();
+const app=express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/transactions",transactionsRoute);
+app.listen(5000,()=>{
+  console.log(server running on port 5000");
+});
+
+
 
